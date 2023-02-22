72a3f338f943c5932bca01523d310d28466bf966
Samyak Bardiya <samyak.b@cumulations.com>
2023-02-22 16:38:30 +0530

    DOM

--- 02__js_in_browser/01__dom_event_and_event_listeners/script.js
+++ 02__js_in_browser/01__dom_event_and_event_listeners/script.js
@@ -1,8 +1,20 @@
-function myFunc() {
-  console.log("Button pressed");
+function myFunc(msg, e) {
+  console.log("=> e:", e);
+  container.textContent = `${msg} ${e}`;
 }
 
 let button = document.getElementById("form-button");
-button.addEventListener("click", myFunc);
 
 let form = document.getElementById("form");
+
+const container = document.querySelector("#container");
+
+// form events
+form.addEventListener("change", (e) => myFunc("Change", e));
+form.addEventListener("reset", (e) => myFunc("Reset", e));
+form.addEventListener("submit", (e) => myFunc("Submit", e));
+
+// mouse events
+window.addEventListener("click", (e) => myFunc("click", e));
+window.addEventListener("contextmenu", (e) => myFunc("contextmenu", e));
+window.addEventListener("dblclick", (e) => myFunc("dblclick", e));
